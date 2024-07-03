4065c9d1241f50c102c7bbd4c2ef1bd3dfad3bc0
Nikolay Danailov <frostblooded@yahoo.com>
2024-07-04 00:07:42 +0300

    Add shadow and height to trash

--- scripts/player_preparing_state.gd
+++ scripts/player_preparing_state.gd
@@ -14,10 +14,14 @@ func exit(parent: Node) -> void:
 func state_process(_delta: float, parent: Node) -> void:
 	if Input.is_action_just_pressed("interact"):
 		var player: Player = parent as Player
-		var direction: Vector2 = Vector2.UP.rotated(player.arrow_texture.rotation)
-		var trash: RigidBody2D = player.trash_scene.instantiate() as RigidBody2D
-		trash.linear_velocity = direction * player.trash_speed
-		trash.position = player.trash_spawn.global_position
-		get_tree().get_root().add_child(trash)
-
+		shoot_trash(player)
 		transitioned.emit(self, "PlayerIdleState")
+
+func shoot_trash(player: Player) -> void:
+	var direction: Vector2 = Vector2.UP.rotated(player.arrow_texture.rotation)
+
+	var trash: Trash = player.trash_scene.instantiate() as Trash
+	trash.linear_velocity = direction * player.trash_speed
+	trash.position = player.trash_spawn.global_position
+
+	get_tree().get_root().add_child(trash)
