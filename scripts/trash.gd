4c19c394daf3aae30ee6fdb21c4a011694c6541c
Nikolay Danailov <frostblooded@yahoo.com>
2024-07-12 00:30:52 +0300

    Rework game to be side-view

--- scripts/trash.gd
+++ scripts/trash.gd
@@ -1,20 +1,5 @@
 class_name Trash
 extends RigidBody2D
 
-@export var shadow: Node2D
-@export var lifetime: float = 2
-
-var _spent_lifetime: float = 0
-var _height: float = 100
-
-func _process(delta: float) -> void:
-	_spent_lifetime += delta
-	shadow.position.y = _height
-
-	var lifetime_percent: float = _spent_lifetime / lifetime
-	var val: float = lifetime_percent - 0.5
-	_height = -(val * val) + 0.3
-	_height *= 300
-
-	if _spent_lifetime >= lifetime:
-		queue_free()
+func freeze_body() -> void:
+	freeze = true
